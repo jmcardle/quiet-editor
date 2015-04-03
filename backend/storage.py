cc33b2fe19a4640ef3eb131e3aa758db082ba7f9
Maëlys <airseal@hotmail.com>
2015-04-03 19:01:56 -0400

    Worked on database.

--- backend/storage.py
+++ backend/storage.py
@@ -1,11 +1,42 @@
 import time
 import threading
-
+import sqlite3
+import zlib
 
 updates = {}
 in_memory_text = {}
 unsaved_changes = {}
 minimum_time_between_saves = 10
+DATABASE_NAME = "storage.db"
+
+def add_revision(database_name, file_name, contents):
+
+    database = sqlite3.connect(database_name)
+    cursor = database.cursor()
+
+    # Create tables if they don't exist.
+    cursor.execute('CREATE TABLE IF NOT EXISTS file_names (id INTEGER PRIMARY KEY, name TEXT, UNIQUE(name));')
+    cursor.execute('CREATE TABLE IF NOT EXISTS file_revisions (id INTEGER PRIMARY KEY, file_id INTEGER, '
+                   'Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, compressed_contents BLOB, '
+                   'FOREIGN KEY (file_id_exists) REFERENCES file_names(id));')
+
+    # Store the file name if it doesn't exist.
+    cursor.execute('INSERT OR IGNORE INTO file_names(name) VALUES(?)', (file_name,))
+
+    # Get the row ID for the file name.
+    cursor.execute('SELECT id FROM file_names WHERE name=?', (file_name,))
+    file_id = cursor.fetchone()[0]
+
+    # Store the compressed text.
+    compressed_text = zlib.compress(contents)
+    cursor.execute('INSERT INTO file_revisions (file_id, compressed_contents) VALUES (?, ?)',
+                   (file_id, compressed_text))
+
+    # Save changes and close.
+    database.commit()
+    database.close()
+
+
 
 def check_unsaved():
     for file_name in unsaved_changes:
