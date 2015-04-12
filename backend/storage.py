1c4c8dd0dac464504f55819e08573577fc7f040f
Maëlys <airseal@hotmail.com>
2015-04-12 09:47:02 -0400

    Updated to safe file names.

--- backend/storage.py
+++ backend/storage.py
@@ -1,6 +1,8 @@
 import os.path
+import base64
 
 files = {}
+file_directory = "../files"
 
 def get(file_name):
 
@@ -9,11 +11,10 @@ def get(file_name):
         return files[file_name]
 
     # Copy exists on disk. Load it in memory.
-    elif os.path.isfile(file_name):
-        with open(file_name, "r") as file:
-            text = file.read()
-            files[file_name] = text
-            return text
+    elif file_exists(file_name):
+        text = read_file(file_name)
+        files[file_name] = text
+        return text
 
     # No copy exists.
     else:
@@ -22,24 +23,44 @@ def get(file_name):
 def update(file_name, text):
 
     # File doesn't exist or isn't in database.
-    if not os.path.isfile(file_name) or file_name not in files:
-        with open(file_name, "w") as file:
-            file.write(text)
+    if not file_exists(file_name) or file_name not in files:
+        write_file(file_name, text)
 
     # Just text is appended.
     elif text.startswith(files[file_name]):
-        with open(file_name, "a") as file:
-            file.write(text[len(files[file_name]):])
+        append_file(file_name, text[len(files[file_name]):])
 
     # Just text is removed.
     elif files[file_name].startswith(text):
-        with open(file_name, "w") as file:
-            file.truncate(len(text))
+        truncate_file(file_name, len(text))
 
     # Content added elsewhere.
     else:
-        with open(file_name, "w") as file:
-            file.write(text)
+        write_file(file_name, text)
 
     # Update memory.
     files[file_name] = text
+
+def write_file(file_name, content, mode="w"):
+    with open(to_safe_filename(file_name), mode) as file:
+        file.write(content)
+
+def append_file(file_name, content):
+    write_file(file_name, content, "a")
+
+def read_file(file_name):
+    with open(to_safe_filename(file_name), "r") as file:
+        return file.read()
+
+def truncate_file(file_name, size):
+    with open(to_safe_filename(file_name), "w") as file:
+        file.truncate(size)
+
+def file_exists(file_name):
+    return os.path.isfile(to_safe_filename(file_name))
+
+def to_safe_filename(file_name):
+    return os.path.join(file_directory, base64.urlsafe_b64encode(file_name.encode()).decode())
+
+def from_safe_filename(file_name):
+    return base64.urlsafe_b64decode(os.path.basename(file_name)).decode()
